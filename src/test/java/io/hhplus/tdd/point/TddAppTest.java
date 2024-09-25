6df0c76b8bf8b72283292de2a7a0e8f5422e888a
unknown <pc95@naver.com>
2024-09-25 20:15:30 +0900

    merge 해소

--- src/test/java/io/hhplus/tdd/point/TddAppTest.java
+++ src/test/java/io/hhplus/tdd/point/TddAppTest.java
@@ -5,13 +5,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-public class tddAppTest {
+@SpringBootTest
+public class TddAppTest {
 
     @Autowired
     PointService pointService;
     @Test
     void pointTest() {
-
+        System.out.println(pointService.charge(1,200));
     }
 }
