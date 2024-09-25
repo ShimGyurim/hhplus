e01055bc6f6494aa25548522b572fe9bbe582585
Shim Gyurim <41977048+ShimGyurim@users.noreply.github.com>
2024-09-25 14:40:54 +0900

    Update TddAppTest.java
    update draft

--- src/test/java/io/hhplus/tdd/point/TddAppTest.java
+++ src/test/java/io/hhplus/tdd/point/TddAppTest.java
@@ -11,7 +11,68 @@ public class tddAppTest {
     @Autowired
     PointService pointService;
     @Test
-    void pointTest() {
+    void lookup1Return100() {
+        UserPoint expectedUserPoint = new UserPoint(id, 100, 0);
+        Mockito.when(userPointTable.selectById(id)).thenReturn(expectedUserPoint);
 
+        // When
+        UserPoint actualUserPoint = pointService.lookup(id);
+
+        // Then
+        assertEquals(expectedUserPoint, actualUserPoint);
+    }    
+
+    @Test
+    void lookup1ReturnNull() {
+        // Given
+        long id = 1L;
+        Mockito.when(userPointTable.selectById(id)).thenReturn(null);
+
+        // When
+        UserPoint actualUserPoint = pointService.lookup(id);
+
+        // Then
+        assertNull(actualUserPoint);
     }
+
+@Test
+public void from100charge100success() {
+    // Given
+    long id = 1L;
+    long amount = 100L;
+
+    UserPoint expectedOriginUserPoint = new UserPoint(id,100,0);
+    UserPoint expectedUserPoint = new UserPoint(id, 200, 0);
+    Mockito.when(userPointTable.selectById(id)).thenReturn(expectedOriginUserPoint);
+    Mockito.when(userPointTable.insertOrUpdate(anyLong(), anyLong())).thenReturn(expectedUserPoint);
+
+    // When
+    UserPoint actualUserPoint = pointService.charge(id, amount);
+
+    // Then
+    assertAll(
+        () -> assertEquals(expectedUserPoint.id,actualUserPoint.id),
+        () -> assertEquals(expectedUserPoint.amount,actualUserPoint.amount
+    );   
+   
+    verify(pointHistoryTable).insert(anyLong(), anyLong(), eq(TransactionType.CHARGE), anyLong());
+}    
+
+@Test
+public void from() {
+    // Given
+    long id = 1L;
+    long amount = 200L;
+    UserPoint expectedUserPoint = new UserPoint(id, 100, 0);
+    Mockito.when(userPointTable.selectById(id)).thenReturn(expectedUserPoint);
+    Mockito.when(userPointTable.insertOrUpdate(anyLong(), anyLong())).thenReturn(expectedUserPoint);
+
+    // When
+    UserPoint actualUserPoint = pointService.use(id, amount);
+
+    // Then
+    assertEquals(expectedUserPoint, actualUserPoint);
+    verify(pointHistoryTable, never()).insert(anyLong(), anyLong(), eq(TransactionType.USE), anyLong());
+}
+
 }
