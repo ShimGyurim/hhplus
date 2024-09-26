5b2771c3a02c51a378719eb8d40ac96de983a5bb
unknown <pc95@naver.com>
2024-09-27 03:06:36 +0900

    항해 제출 전

--- src/test/java/io/hhplus/tdd/point/TddIntegTest.java
+++ src/test/java/io/hhplus/tdd/point/TddIntegTest.java
@@ -1,6 +1,8 @@
 package io.hhplus.tdd.point;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
@@ -8,9 +10,12 @@ import org.springframework.stereotype.Service;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest
 public class TddTonghapTest {
+
+    private static final Logger log = LoggerFactory.getLogger(PointController.class);
 
     @Autowired
     PointService pointService;
@@ -26,11 +31,17 @@ public class TddTonghapTest {
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         CountDownLatch latch = new CountDownLatch(threadCount * iterationCount);
 
+        ThreadLocal<Integer> threadNumber = new ThreadLocal<>();
+        AtomicInteger globalThreadCount = new AtomicInteger(1);
+
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
+                int currentThreadNumber = globalThreadCount.getAndIncrement();
+                threadNumber.set(currentThreadNumber);
+
                 for (int j = 0; j < iterationCount; j++) {
-                    pointService.charge(1L, 100L); // 예시: 사용자 ID 1, 충전 금액 100
-                    System.out.println("test:"+"" +"and"+ j);
+                    UserPoint userPoint = pointService.charge(1L, 100L);
+                    log.debug(userPoint+" 스레드"+threadNumber.get()+"의"+j+"번째"); // 예시: 사용자 ID 1, 충전 금액 100
                     latch.countDown();
                 }
             });
